45bfc24067ac1fdd97c37bd78ec501fe3543e089
Maciej Socha <maciejsocha1@gmail.com>
2019-01-10 00:17:03 +0100

    added dynamic creation of VBoxes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -22,18 +24,41 @@ public class Main extends Application {
     private static GridPane root = new GridPane();
     private static BorderPane borderPane = new BorderPane();
     private static ScrollPane sp = new ScrollPane(root);
+    private static Button countDays = new Button("Count Trip Length");
+    private static DatePicker startDatePicker = new DatePicker();
+    private static DatePicker endDatePicker = new DatePicker();
+    private static TextField startTimeTextField = new TextField();
+    private static TextField endTimeTextField = new TextField();
+
+
+
+    private static int daysEntitled;
     private static long diff;
     private int row;
+    private int columnIndex=0;
+    private int rowIndex=1;
+    private long reminder;
 
     private static ArrayList<MojeBoxy> boxy = new ArrayList<>();
 
 
     @Override
     public void start(Stage primaryStage)  {
+        countDays.setOnAction(event -> {
+
+            daysEntitled = (checkTravelTime());
+            centerScrollPane(daysEntitled);
+            remainderDays(row, reminder);
+
+        });
+
+
+
+
 
         dateAndTimeVBox();
-       bottomButtons();
-        centerScrollPane();
+        bottomButtons();
+
 
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(borderPane, 1280, 720));
@@ -49,11 +74,9 @@ public class Main extends Application {
         BorderPane.setAlignment(leftSideVBox, Pos.CENTER);
         leftSideVBox.setAlignment(Pos.TOP_CENTER);
         Label startDateLabel = new Label("Start Date:");
-        DatePicker startDatePicker = new DatePicker();
         startDatePicker.prefHeight(27);
         startDatePicker.setPrefWidth(150);
         Label startTimeLabel = new Label("Start Time:");
-        TextField startTimeTextField = new TextField();
         startTimeTextField.setPromptText("00:00");
         startTimeTextField.setAlignment(Pos.CENTER);
         VBox.setMargin(startDateLabel, new Insets(50,0,0,0));
@@ -63,11 +86,9 @@ public class Main extends Application {
         leftSideVBox.getChildren().add(startTimeLabel);
         leftSideVBox.getChildren().add(startTimeTextField);
         Label endDateLabel = new Label("End Date:");
-        DatePicker endDatePicker = new DatePicker();
         endDatePicker.prefHeight(27);
         endDatePicker.setPrefWidth(150);
         Label endTimeLabel = new Label("End Time:");
-        TextField endTimeTextField = new TextField();
         endTimeTextField.setPromptText("00:00");
         endTimeTextField.setAlignment(Pos.CENTER);
         VBox.setMargin(endDateLabel, new Insets(50,0,0,0));
@@ -79,21 +100,29 @@ public class Main extends Application {
     }
 
 
-    private void centerScrollPane(){
+    private void centerScrollPane(int daysEntitledToTravel){
        borderPane.setCenter(sp);
         sp.setFitToWidth(true);
 
-        int i=0;
-        int k=1;
-        Region rgn = new Region();
-        rgn.setMinHeight(150);
-        root.add(rgn, 0, 0);
-        for (row = 1; row <= 50; row++) {
+
+
+        for (row = 0; row < daysEntitledToTravel; row++) {
+            columnIndex++;
+
+            if(row%7==0){
+                rowIndex+=4;
+                columnIndex=0;
+                Region rgn = new Region();
+                rgn.setMinHeight(75);
+                root.add(rgn, 0, rowIndex);
+                rowIndex++;
+
+            }
             Label btn = new Label();
             btn.setWrapText(true);
-            btn.setText("Day " + row);
+            btn.setText("Day " + (row+1));
             btn.setMinSize(75, 15);
-            root.add(btn, row,1 );
+            root.add(btn, columnIndex,rowIndex);
             root.setHgap(25);
 
             for (int j = 0; j < 3; j++) {
@@ -101,17 +130,17 @@ public class Main extends Application {
                 //
                 CheckBox breakfast = new CheckBox();
                 breakfast.setText("Breakfast");
-                root.add(breakfast, row, 5);
+                root.add(breakfast, columnIndex, rowIndex+1);
 
                 //
                 CheckBox lunch = new CheckBox();
                 lunch.setText("Lunch");
-                root.add(lunch, row, 6);
+                root.add(lunch, columnIndex, rowIndex+2);
 
                 //
                 CheckBox dinner = new CheckBox();
                 dinner.setText("Dinner");
-                root.add(dinner, row, 7);
+                root.add(dinner, columnIndex, rowIndex+3);
 
                 boxy.add(new MojeBoxy(breakfast, lunch, dinner));
             }
@@ -126,13 +155,45 @@ public class Main extends Application {
         HBox bottomHBox = new HBox();
         borderPane.setBottom(bottomHBox);
 
-        Button countDays = new Button("Count Trip Length");
+
         bottomHBox.getChildren().add(countDays);
         bottomHBox.setAlignment(Pos.TOP_LEFT);
-
-
-
     }
+
+
+private int checkTravelTime() {
+    String format = "yyyy-MM-ddHH:mm";
+    SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.GERMANY);
+    int days =0;
+
+        try {
+            String date1 = startDatePicker.getValue().toString().trim();
+            String date2 = endDatePicker.getValue().toString().trim();
+            Date dateObj1 = sdf.parse(date1 + "" + String.valueOf(startTimeTextField.getText()));
+            Date dateObj2 = sdf.parse(date2 + " " + String.valueOf(endTimeTextField.getText()));
+            System.out.println(dateObj1);
+            System.out.println(dateObj2 + "\n");
+            diff = ((dateObj2.getTime() - dateObj1.getTime()));
+            System.out.println(diff);
+
+            days = (int) (diff / 86_400_000);
+            reminder = diff % 86_400_000;
+
+
+           // checkIfMealsSelected(boxy, days);
+
+            System.out.println("days entitled: " + days + "reminder: " + reminder);
+
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+        return days;
+
+}
+
+
+
+
 
 
 
@@ -250,90 +311,106 @@ public class Main extends Application {
 //        }
 //    }
 //
-//    private void remainderDays(int row, long remainder) {
-//
-//        //remainder for less than 8hours
-//        if (remainder < 28_800_001 && remainder > 0) {
-//            Label btn = new Label();
-//            btn.setWrapText(true);
-//            btn.setText("Day " + row );
-//            btn.setMinSize(75, 15);
-//            root.add(btn, row + 1, 4);
-//            root.setHgap(25);
-//            CheckBox breakfast = new CheckBox();
-//            breakfast.setText("Breakfast for day <8h");
-//            root.add(breakfast, row + 1, 5);
-//
-//            //
-//            CheckBox lunch = new CheckBox();
-//            lunch.setText("Lunch for day <8h");
-//            root.add(lunch, row + 1, 6);
-//
-//            //
-//            CheckBox dinner = new CheckBox();
-//            dinner.setText("Dinner for day <8h");
-//            root.add(dinner, row + 1, 7);
-//
-//
-//            boxy.add(new MojeBoxy(breakfast, lunch, dinner));
-//        }
-//
-//        //remainder for more than 8hours but less than 12h
-//        if (remainder > 28_800_001 && remainder < 43_200_000) {
-//            Label btn = new Label();
-//            btn.setWrapText(true);
-//            btn.setText("Day " + row );
-//            btn.setMinSize(75, 15);
-//            root.add(btn, row + 1, 4);
-//            root.setHgap(25);
-//            CheckBox breakfast = new CheckBox();
-//            breakfast.setText("Breakfast for day 8-12h");
-//            root.add(breakfast, row + 1, 5);
-//
-//            //
-//            CheckBox lunch = new CheckBox();
-//            lunch.setText("Lunch for day 8-12h");
-//            root.add(lunch, row + 1, 6);
-//
-//            //
-//            CheckBox dinner = new CheckBox();
-//            dinner.setText("Dinner for day 8-12h");
-//            root.add(dinner, row + 1, 7);
-//
-//
-//            boxy.add(new MojeBoxy(breakfast, lunch, dinner));
-//        }
-//
-//        if (remainder >= 43_200_000 && remainder < 86_400_000) {
-//            Label btn = new Label();
-//            btn.setWrapText(true);
-//            btn.setText("Day " + row);
-//            btn.setMinSize(75, 15);
-//            root.add(btn, row, 4);
-//            root.setHgap(25);
-//            for (int j = 0; j < 3; j++) {
-//
-//                //
-//                CheckBox breakfast = new CheckBox();
-//                breakfast.setText("Breakfast 12h travel");
-//                root.add(breakfast, row, 5);
-//
-//                //
-//                CheckBox lunch = new CheckBox();
-//                lunch.setText("Lunch 12h travel");
-//                root.add(lunch, row, 6);
-//
-//                //
-//                CheckBox dinner = new CheckBox();
-//                dinner.setText("Dinner 12h travel");
-//                root.add(dinner, row, 7);
-//
-//                System.out.println(row);
-//                boxy.add(new MojeBoxy(breakfast, lunch, dinner));
-//            }
-//        }
-//    }
-//
+    private void remainderDays(int row, long remainder) {
+        columnIndex++;
+        row++;
+        //remainder for less than 8hours
+        if (remainder < 28_800_001 && remainder > 0) {
+            Label btn = new Label();
+            btn.setWrapText(true);
+            btn.setText("Day " + row );
+            btn.setMinSize(75, 15);
+            GridPane.setColumnSpan(btn,2);
+            root.add(btn, columnIndex, rowIndex);
+            root.setHgap(25);
+            CheckBox breakfast = new CheckBox();
+            breakfast.setText("Breakfast for day <8h");
+            GridPane.setColumnSpan(breakfast,2);
+            root.add(breakfast, columnIndex, rowIndex+1);
+
+            //
+            CheckBox lunch = new CheckBox();
+            lunch.setText("Lunch for day <8h");
+            GridPane.setColumnSpan(lunch,2);
+            root.add(lunch, columnIndex, rowIndex+2);
+
+            //
+            CheckBox dinner = new CheckBox();
+            dinner.setText("Dinner for day <8h");
+            GridPane.setColumnSpan(dinner,2);
+            root.add(dinner, columnIndex, rowIndex+3);
+
+
+            boxy.add(new MojeBoxy(breakfast, lunch, dinner));
+        }
+
+        //remainder for more than 8hours but less than 12h
+        if (remainder > 28_800_001 && remainder < 43_200_000) {
+            Label btn = new Label();
+            btn.setWrapText(true);
+            btn.setText("Day " + row );
+            btn.setMinSize(75, 15);
+            GridPane.setColumnSpan(btn,2);
+
+            root.add(btn, columnIndex, rowIndex);
+            root.setHgap(25);
+            CheckBox breakfast = new CheckBox();
+            breakfast.setText("Breakfast for day 8-12h");
+            GridPane.setColumnSpan(breakfast,2);
+
+            root.add(breakfast, columnIndex, rowIndex+1);
+
+            //
+            CheckBox lunch = new CheckBox();
+            lunch.setText("Lunch for day 8-12h");
+            GridPane.setColumnSpan(lunch,2);
+            root.add(lunch, columnIndex, rowIndex+2);
+
+            //
+            CheckBox dinner = new CheckBox();
+            dinner.setText("Dinner for day 8-12h");
+            GridPane.setColumnSpan(dinner,2);
+            root.add(dinner, columnIndex, rowIndex+3);
+
+
+            boxy.add(new MojeBoxy(breakfast, lunch, dinner));
+        }
+
+        if (remainder >= 43_200_000 && remainder < 86_400_000) {
+            Label btn = new Label();
+            btn.setWrapText(true);
+            btn.setText("Day " + row);
+            btn.setMinSize(75, 15);
+            GridPane.setColumnSpan(btn,2);
+
+            root.add(btn, columnIndex, rowIndex);
+            root.setHgap(25);
+            for (int j = 0; j < 3; j++) {
+
+                //
+                CheckBox breakfast = new CheckBox();
+                breakfast.setText("Breakfast 12h travel");
+                GridPane.setColumnSpan(breakfast,2);
+                root.add(breakfast, columnIndex, rowIndex+1);
+
+                //
+                CheckBox lunch = new CheckBox();
+                lunch.setText("Lunch 12h travel");
+                GridPane.setColumnSpan(lunch,2);
+                root.add(lunch, columnIndex, rowIndex+2);
+
+                //
+                CheckBox dinner = new CheckBox();
+                dinner.setText("Dinner 12h travel");
+                GridPane.setColumnSpan(dinner,2);
+                root.add(dinner, columnIndex, rowIndex+3);
+
+               // System.out.println(row);
+                boxy.add(new MojeBoxy(breakfast, lunch, dinner));
+            }
+        }
+    }
+
 //    private void checkIfMealsSelected(ArrayList<MojeBoxy> arrayList, int days) {
 //
 //        int bNum = 0, lNum = 0, dNum = 0;
